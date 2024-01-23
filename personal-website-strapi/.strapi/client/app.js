95ea33722d9391cddc87c4f8e6f920ff0d3ab2d8
Cong Le <congldqn888@gmail.com>
2024-01-24 01:36:15 +0700

    Add import export entries plugin

--- personal-website-strapi/.strapi/client/app.js
+++ personal-website-strapi/.strapi/client/app.js
@@ -7,6 +7,7 @@ import graphql from "@strapi/plugin-graphql/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import seo from "@strapi/plugin-seo/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
+import importExportEntries from "strapi-plugin-import-export-entries/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 renderAdmin(document.getElementById("strapi"), {
@@ -16,5 +17,6 @@ renderAdmin(document.getElementById("strapi"), {
     i18n: i18N,
     seo: seo,
     "users-permissions": usersPermissions,
+    "import-export-entries": importExportEntries,
   },
 });
